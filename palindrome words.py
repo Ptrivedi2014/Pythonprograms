d0f5b177653e3c71c940da5e89ebe06b91a97c3e
Ptrivedi2014 <Pradyumnatrivedi2014@gmail.com>
2024-11-22 18:07:43 +0530

    Add files via upload

--- palindrome words.py
+++ palindrome words.py
@@ -2,7 +2,7 @@ import time
 while True:
     str=input("enter a word \n")
     print(str[::-1])
-    if str==str[0::]:
+    if str==str[::-1]:
         print(str,"is palindrome")
     else:
         print(str,"is not palindrome")
